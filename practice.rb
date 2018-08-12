b53f7688a0460248b85a081f85c8cd69d53a6b43
darnell ghee <jasmine.blackmon@gmail.com>
2018-08-12 15:19:34 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -5,7 +5,15 @@ def morning_plan
 end
 
 def wake_up(day)
-  if day.downcase.startwith? "s"
+  if day.downcase.start_with? "t"
     puts "its probaly a Tuesday or Thursday" 
-    
+  elsif day.downcase.started_with? "s"
+    puts "Its probaably the weekend"
+  else 
+    puts "Its a Monday, Wednesday or Friday" 
+  end 
+end 
+
+binding.pry
+puts "BYE"
   
